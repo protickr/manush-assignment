45a51d66dd4605597a649cea5992399604481b59
Protick Roy <protickrr@gmail.com>
2026-03-09 12:37:23 +0600

    feat(api): add redis and config modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
+import { RedisModule } from '@cache/redis.module';
+import redisConfig from '@config/redis.config';
 
 @Module({
-  imports: [],
+  imports: [
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [redisConfig],
+    }),
+    RedisModule,
+  ],
   controllers: [AppController],
   providers: [],
 })
